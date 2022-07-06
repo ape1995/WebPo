8be8aeb01dbc7c7da83b80a0d84aa258091bb4eb
Hidayatul Fahrozy <hidayatulfahrozy@gmail.com>
2022-07-06 09:14:37 +0700

    update migration

--- database/migrations/2022_07_01_141441_add_order_nbr_merge_to_sales_orders_table.php
+++ database/migrations/2022_07_01_141441_add_order_nbr_merge_to_sales_orders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\SalesOrder;
 
 class AddOrderNbrMergeToSalesOrdersTable extends Migration
 {
@@ -17,6 +18,14 @@ class AddOrderNbrMergeToSalesOrdersTable extends Migration
             $table->string('order_nbr_merge')->nullable();
             $table->integer('order_merged_by')->nullable();
         });
+
+        $modelCollection = SalesOrder::all();
+		foreach($modelCollection as $model) {
+			//Do your calculation whit old columns and save it in the new column.
+			$model->order_nbr_merge = $model->order_nbr;
+			$model->order_merged_by = 2;
+			$model->save();
+		}
     }
 
     /**
